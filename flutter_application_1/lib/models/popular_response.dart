55ca17aec9b1acd111dbe1bdb2c6afc224f57a8e
Juan Felipe Garcia <97085649+JuanFGQ@users.noreply.github.com>
2023-09-08 20:39:24 +0200

    Update popular_response.dart

--- flutter_application_1/lib/models/popular_response.dart
+++ flutter_application_1/lib/models/popular_response.dart
@@ -8,9 +8,7 @@ import 'package:flutter_application_1/models/models.dart';
 
 class PopularResponse {
   PopularResponse({
-//****leer de corrido todos los comentarios de aqui hacia abajo */
 
-    //estos son los datos que recibo
     required this.page,
     required this.results,
     required this.totalPages,
@@ -22,11 +20,9 @@ class PopularResponse {
   int totalPages;
   int totalResults;
 
-  //******Lo leo del mapa */
   factory PopularResponse.fromJson(String str) =>
       PopularResponse.fromMap(json.decode(str));
 
-  //******Luego hago la conversion de cada uno de los resultados a instacias de mi mapa */
   factory PopularResponse.fromMap(Map<String, dynamic> json) => PopularResponse(
         page: json["page"],
         results: List<Movie>.from(json["results"].map((x) => Movie.fromMap(x))),
