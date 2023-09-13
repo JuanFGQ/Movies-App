7885724b6e6c33a92ea31bfd92a039849b075302
juanFGQ <juanfelipegq@outlook.es>
2023-09-13 11:48:55 +0200

    api call implementation

--- flutter_application_1/lib/clean_architecture/features/main/data/repository/movies_api_implementation.dart
+++ flutter_application_1/lib/clean_architecture/features/main/data/repository/movies_api_implementation.dart
@@ -5,8 +5,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/clean_architecture/core/constants/constanst.dart';
 import 'package:flutter_application_1/clean_architecture/core/resources/data_state.dart';
 import 'package:flutter_application_1/clean_architecture/features/main/data/data_sources/remote/movie_api_service.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/data/models/credits_model.dart';
 import 'package:flutter_application_1/clean_architecture/features/main/data/models/movie_model.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/data/models/popular_model.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/entities/credits_entity.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/entities/popular_entity.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/entities/search_entity.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/repositories/credits_repository.dart';
 import 'package:flutter_application_1/clean_architecture/features/main/domain/repositories/movie_repository.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/repositories/popular_repository.dart';
+import 'package:flutter_application_1/clean_architecture/features/main/domain/repositories/search%20repository.dart';
 
 class MoviesRepositoryImpl implements MovieRepository {
   final MoviesApiService _movieApiService;
@@ -24,33 +32,58 @@ class MoviesRepositoryImpl implements MovieRepository {
     if (response.statusCode == 200) {
       return DataSuccess(movies);
     } else {
-      return DataFailed(Exception('failed request status != 200 '));
+      return DataFailed(Exception('failed request status'));
     }
   }
 }
 
-// class MoviesRepositoryImpl implements MovieRepository {
-//   final MoviesApiService _movieApiService;
+class PopularMoviesRepositoryImpl implements PopularMovieRepository {
+  final MoviesApiService _moviesApiService;
 
-//   MoviesRepositoryImpl(this._movieApiService);
+  PopularMoviesRepositoryImpl(this._moviesApiService);
+
+  @override
+  Future<DataState<List<PopularMoviesModel>>> getPopularMovies() async {
+    final response = await _moviesApiService.getPopularMovies();
+    final popular = (response.data['results'] as List)
+        .map((data) => PopularMoviesModel.fromJson(data))
+        .toList();
+
+    if (response.statusCode == 200) {
+      return DataSuccess(popular);
+    } else {
+      return DataFailed(Exception('Failed request'));
+    }
+  }
+}
+
+class MovieCastRepositoryImp implements CreditsRepository {
+  final MoviesApiService _moviesApiService;
+
+  MovieCastRepositoryImp(this._moviesApiService);
+
+  @override
+  Future<DataState<List<CreditsEntity>>> getMovieCredits() async {
+    final response = await _moviesApiService.getMovieCastById(1);
+    final credits = (response.data['results'] as List)
+        .map((data) => CastModel.fromJson(data))
+        .toList();
+
+    if (response.statusCode == 200) {
+      return DataSuccess(credits);
+    } else {
+      return DataFailed(Exception('Failed request'));
+    }
+  }
+}
+//  class SearchMovieRepositoryImpl implements SearchRepository{
+//   final MoviesApiService _moviesApiService;
 
 //   @override
-//   Future<DataState<List<MovieModel>>> getMovieArticles() async {
-//     try {
-//       final httpResponse = await _movieApiService.getMovies(apiKey: apiKey);
+//   Future<DataState<List<SearchEntity>>> getSearch() {
+// final response = await _movieApiService
 
-//       if (httpResponse.response.statusCode == HttpStatus.ok) {
-//         return DataSuccess(httpResponse.data);
-//       } else {
-//         return DataFailed(DioException(
-//           requestOptions: httpResponse.response.requestOptions,
-//           error: httpResponse.response.statusMessage,
-//           type: DioExceptionType.badResponse,
-//           response: httpResponse.response,
-//         ));
-//       }
-//     } on DioException catch (e) {
-//       return DataFailed(e);
-//     }
+
 //   }
-// }
+
+//   }
