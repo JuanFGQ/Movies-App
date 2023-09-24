8d7f603d3e4980acf0b0250813ec64c843931376
JuanFGQ <juanfelipegq@outlook.es>
2023-09-24 18:07:41 +0200

    provider deleted

--- flutter_application_1/lib/Provider manage/screens/Screens.dart
+++
@@ -1,3 +0,0 @@
-// export 'package:flutter_application_1/screens/details_screens.dart';
-// export 'package:flutter_application_1/screens/home_screen.dart';
-// export 'package:flutter_application_1/Provider%20manage/widgets/widgets.dart';
