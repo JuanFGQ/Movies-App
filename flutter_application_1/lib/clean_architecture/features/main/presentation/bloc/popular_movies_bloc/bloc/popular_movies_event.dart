e7674941e851860a2b466591e9686596f2fc0e90
JuanFGQ <juanfelipegq@outlook.es>
2023-09-25 13:30:46 +0200

    new event created for new list, isn´t in use

--- flutter_application_1/lib/clean_architecture/features/main/presentation/bloc/popular_movies_bloc/bloc/popular_movies_event.dart
+++ flutter_application_1/lib/clean_architecture/features/main/presentation/bloc/popular_movies_bloc/bloc/popular_movies_event.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_application_1/clean_architecture/features/main/domain/entities/movie_entity.dart';
+
 abstract class PopularMoviesEvent {
-  const PopularMoviesEvent();
+  // final List<MovieEntity>? onGetPopularMovies =;
+
+  // PopularMoviesEvent(this.onGetPopularMovies);
 }
 
-class GetPopularMovies extends PopularMoviesEvent {
-  const GetPopularMovies();
-}
+class GetPopularMovies extends PopularMoviesEvent {}
+
+class AssgingPopularMovies extends PopularMoviesEvent {}
